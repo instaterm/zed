3e68eec2f571d61ef3de45346d08b86ddb5cf7f5
Wilbur Hu <wilburhu83@gmail.com>
2025-12-15 10:46:31 +0800

    fix: recalculate layout.width for force_width

--- crates/gpui/src/text_system/line_layout.rs
+++ crates/gpui/src/text_system/line_layout.rs
@@ -575,6 +575,7 @@ impl LineLayoutCache {
                         glyph_pos += 1;
                     }
                 }
+                layout.width = glyph_pos * force_width;
             }
 
             let key = Arc::new(CacheKey {
